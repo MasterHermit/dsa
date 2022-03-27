3a2e043f5d05c20089223da9f0ea15be6ef709cc
MasterHermit <rajpruthwi5534.ps@gmail.com>
2022-03-27 20:36:27 +0530

    printing all sub sequences of a string

---
+++ practice_recurssion/AllSubsequencesOfAString.java
@@ -0,0 +1,19 @@
+public class AllSubsequencesOfAString {
+    static void printSS(String s,String ans) {
+       
+        if(s.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        char ch=s.charAt(0);
+        String rest=s.substring(1);
+        printSS(rest,ans+"");
+        printSS(rest,ans+ch);
+    }
+    public static void main(String[] args) {
+        String s="abc";
+        String ans="";
+        printSS(s, ans);
+    }
+    
+}
