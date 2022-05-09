808c52c0db67054ba218878c4611cbe032b576fc
MasterHermit <rajpruthwi5534.ps@gmail.com>
2022-05-09 17:35:55 +0530

    bfs without source

--- practice_graph/AdjancecyList.java
+++ practice_graph/AdjancecyList.java
@@ -14,8 +14,8 @@ public class AdjancecyList {
             System.out.println();
         }
     }
-    static void bfs(ArrayList<ArrayList<Integer>> al,int v,int s){
-        boolean[] arr=new boolean[v+1];
+    static void bfs(ArrayList<ArrayList<Integer>> al,int s,boolean[] arr){
+        
         Queue<Integer> q = new LinkedList<>();
         q.add(s);
         arr[s]=true;
@@ -30,6 +30,14 @@ public class AdjancecyList {
             }
         }
     }
+    static void preBfs(ArrayList<ArrayList<Integer>> al,int v){
+        boolean[] arr=new boolean[v+1];
+        for(int s=0;s<v;s++){
+            if(arr[s]==false){
+                bfs(al, s, arr);
+            }
+        }
+    }
     public static void main(String[] args) {
         int v = 5;
         ArrayList<ArrayList<Integer>> al = new ArrayList<ArrayList<Integer>>(v);
@@ -38,11 +46,11 @@ public class AdjancecyList {
 
             addEdge(al,0,1);
             addEdge(al,0,2);
-            addEdge(al,1,2);
+            addEdge(al,1,4);
             addEdge(al,1,3);
             printGraph(al);
             int s=0;
-            bfs(al, v, s);
+            preBfs(al, v);
         
             
     }
